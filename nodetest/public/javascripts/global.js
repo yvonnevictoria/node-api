7467a0d876e950f98577a7656aa71e50736723d1
yvonnevictoria <owenyvonne@gmail.com>
2018-08-20 10:03:42 +1000

    Implement add user functionality

--- nodetest/public/javascripts/global.js
+++ nodetest/public/javascripts/global.js
@@ -9,7 +9,9 @@ $(document).ready(function() {
 
   // Username link click
   $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
-
+	 
+  // Add User button click
+  $('#btnAddUser').on('click', addUser);
 });
 
 // Functions =============================================================
@@ -60,4 +62,60 @@ function showUserInfo(event) {
   $('#userInfoAge').text(thisUserObject.age);
   $('#userInfoGender').text(thisUserObject.gender);
   $('#userInfoLocation').text(thisUserObject.location);
+};
+
+// Add User
+function addUser(event) {
+  event.preventDefault();
+
+  // Super basic validation - increase errorCount variable if any fields are blank
+  var errorCount = 0;
+  $('#addUser input').each(function(index, val) {
+    if($(this).val() === '') { errorCount++; }
+  });
+
+  // Check and make sure errorCount's still at zero
+  if(errorCount === 0) {
+
+    // If it is, compile all user info into one object
+    var newUser = {
+      'username': $('#addUser fieldset input#inputUserName').val(),
+      'email': $('#addUser fieldset input#inputUserEmail').val(),
+      'fullname': $('#addUser fieldset input#inputUserFullname').val(),
+      'age': $('#addUser fieldset input#inputUserAge').val(),
+      'location': $('#addUser fieldset input#inputUserLocation').val(),
+      'gender': $('#addUser fieldset input#inputUserGender').val()
+    }
+
+    // Use AJAX to post the object to our adduser service
+    $.ajax({
+      type: 'POST',
+      data: newUser,
+      url: '/users/adduser',
+      dataType: 'JSON'
+    }).done(function( response ) {
+
+      // Check for successful (blank) response
+      if (response.msg === '') {
+
+        // Clear the form inputs
+        $('#addUser fieldset input').val('');
+
+        // Update the table
+        populateTable();
+
+      }
+      else {
+
+        // If something goes wrong, alert the error message that our service returned
+        alert('Error: ' + response.msg);
+
+      }
+    });
+  }
+  else {
+    // If errorCount is more than 0, error out
+    alert('Please fill in all fields');
+    return false;
+  }
 };
--- nodetest/routes/users.js
+++ nodetest/routes/users.js
@@ -10,5 +10,16 @@ router.get('/userlist', function(req, res) {
   });
 });
 
+/* POST to adduser. */
+router.post('/adduser', function(req, res) {
+  var db = req.db;
+  var collection = db.get('userlist');
+  collection.insert(req.body, function(err, result){
+    res.send(
+      (err === null) ? { msg: '' } : { msg: err }
+    );
+  });
+});
+
 module.exports = router;
 
