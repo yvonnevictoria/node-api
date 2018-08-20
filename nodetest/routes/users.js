7abc3014e7a8deb8ff7e1cc40c9089671d2cdb69
yvonnevictoria <owenyvonne@gmail.com>
2018-08-20 10:19:12 +1000

    Implement delete user functionality

--- nodetest/routes/users.js
+++ nodetest/routes/users.js
@@ -21,5 +21,15 @@ router.post('/adduser', function(req, res) {
   });
 });
 
+/* DELETE to deleteuser. */
+router.delete('/deleteuser/:id', function(req, res) {
+  var db = req.db;
+  var collection = db.get('userlist');
+  var userToDelete = req.params.id;
+  collection.remove({ '_id' : userToDelete }, function(err) {
+    res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+  });
+});
+
 module.exports = router;
 
